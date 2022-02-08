259c33e65cd04be5a328046379038b6d1bbe62ef
winstonwang <wwang@lcs.on.ca>
2022-02-07 20:13:22 -0500

    Initial Commit

---
+++ ArithmeticAce/ArithmeticAceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticAceApp.swift
+//  ArithmeticAce
+//
+//  Created by Winston Wang on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticAceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArithmeticAce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArithmeticAce
+//
+//  Created by Winston Wang on 2022-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
